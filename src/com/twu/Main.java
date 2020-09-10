6000ccb753d9ff4f180cb6a0a05a7cf7005f5168
Suerta <suerta__@hotmail.com>
2020-09-10 14:20:50 +0800

    [Zhang Jindong] feat: adjust control flow

--- src/com/twu/Main.java
+++ src/com/twu/Main.java
@@ -1,8 +1,20 @@
 package com.twu;
 
+import com.twu.page.HomePage;
+import com.twu.page.Page;
+import com.twu.page.UserPage;
+
 public class Main {
 
     public static void main(String[] args) {
+        UserContainer userContainer = new UserContainer();
+        EventRankingList eventRankingList = new EventRankingList();
+        UIController uiController = new UIController();
 
+        Page homePage = new HomePage();
+        Page userPage = new UserPage(userContainer, eventRankingList);
+
+        uiController.registerPages(homePage, userPage);
+        uiController.run();
     }
 }
